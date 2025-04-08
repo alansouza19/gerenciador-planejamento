bff6ed8508d1daffce1cc0b74a2b061c768b1d9e
alansouza19 <alan.souza-97@hotmail.com>
2025-04-08 13:54:54 -0300

    Aprendendo a usar hooks (useState)

--- gerenciador-planejamento-projetos/src/components/Form.js
+++ gerenciador-planejamento-projetos/src/components/Form.js
@@ -1,16 +1,36 @@
+import {useState} from 'react'
+
 function Form(){
 
     function cadastrarUser(e){
         e.preventDefault()
-        console.log("User cadastrado")
+        console.log(`User ${name} foi cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return(
         <div>
             <h1>Faça seu cadastro aqui</h1>
             <form onSubmit={cadastrarUser}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome completo"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text"
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite seu nome"
+                        onChange={(e) => setName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite sua senha"
+                        onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar"/>
