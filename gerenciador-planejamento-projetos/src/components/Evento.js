adcd5910a8ac506151592c9c77c19aa217e5caa1
alansouza19 <alan.souza-97@hotmail.com>
2025-04-09 11:03:38 -0300

    Aprendendo a passar métodos por props.

--- gerenciador-planejamento-projetos/src/components/Evento.js
+++ gerenciador-planejamento-projetos/src/components/Evento.js
@@ -1,13 +1,20 @@
 import styles from './Evento.module.css'
+import Button from './evento/Button'
 
 function Evento(){
     
     function meuEvento(){
-        alert("Parabéns, você criou uma função!");
+        alert("Parabéns, você chamou um evento!");
+    }
+
+    function segundoEvento(){
+        alert("Ativando segundo evento!");
     }
 
     return(
         <>
+        <Button event={meuEvento} text="Primeiro evento"/>
+        <Button event={segundoEvento} text="Segundo evento"/>
         <p>Clique aqui para disparar um evento.</p>
         <button className={styles.botaoClique} onClick={meuEvento} >Clique no botão</button>
         </>
